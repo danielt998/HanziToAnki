eba37ea41fd2e011cc438e318685f615469ce7fc
Daniel Thomas <daniel.thomas-6@student.manchester.ac.uk>
2017-05-11 23:55:08 +0100

    started with command line argument handling, now have -f flag =D

--- HanziToAnki/ConvertToCSV.java
+++ HanziToAnki/ConvertToCSV.java
@@ -42,11 +42,39 @@ public class ConvertToCSV{
     return lines.toArray(new String[0]);
   }
 
-  private static boolean useWordList=true;
+  /*last argument is file name, others are all modifiers:
+   * -f:file containing words separated by spaces, otherwise defaults to individual chars
+   */
+  public static void printUsage(){
+    System.out.println("Usage: java ConvertToCSV [-f] filename");
+    System.out.println("arguments:");
+    System.out.println("\t-f:\tRead from an input file containing a list of words, separated"+
+                        " by line breaks. Without this flag, individual characters are extracted.");
+  }
+  
   public static void main(String[] args){
+    if(args.length==0||args[0].equals("-h")){
+      printUsage();
+      return;
+    }
+    boolean useWordList=false;
+    String filename = args[args.length-1];
+    for(int argno=0;argno<args.length-1;argno++){
+      //flag handling
+      if(args[argno].equals("-f")){
+        useWordList=true;
+      }else{
+        System.out.println(
+                       "An unrecognised flag was used, please see below for usage information:\n");
+        printUsage();
+        return;
+      }
+      //handle other flags..., create a separate class if args get too numerous
+    }
+
     if(useWordList){
       Extract extract = new Extract();
-      String[] stringArr=getWordsFromFile(args[0]);
+      String[] stringArr=getWordsFromFile(filename);
       for(int i=0;i<stringArr.length;i++){
         String s=stringArr[i];
         System.out.println(i+";"+s + ";" + extract.getEnglish(s)
@@ -56,7 +84,7 @@ public class ConvertToCSV{
     }else{
       Extract extract = new Extract();
       //char[] charArray=getChars();
-      char[] charArray=getCharsFromFile(args[0]);
+      char[] charArray=getCharsFromFile(filename);
       for(int i=0;i<charArray.length;i++){
         char c=charArray[i];
         System.out.println(i+";"+c + ";" + extract.getEnglish(""+c)
