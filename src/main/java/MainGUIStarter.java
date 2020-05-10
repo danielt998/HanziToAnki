f495eed582b6cd8f108e0e52663ffbacbba0eca7
James <IdiosApps@users.noreply.github.com>
2020-05-10 08:35:58 +0100

    [MainGUIStarter] Starter class calls the MainGUI - workaround due to JavaFX loading issues

---
+++ src/main/java/MainGUIStarter.java
@@ -0,0 +1,12 @@
+public class MainGUIStarter {
+
+    /**
+     * MainGUI extends application, and need doesn't load javafx well (even with jvm args for javafx set in build.gradle)
+     * So this helper class loads javafx, then starts the javafx GUI
+     * @param args can be empty
+     */
+    public static void main(String[] args) {
+        MainGUI gui = new MainGUI();
+        gui.main("");
+    }
+}
