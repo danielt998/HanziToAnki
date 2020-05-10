15713755d49574236feb3c5913a1fc3f3682bf6f
James <IdiosApps@users.noreply.github.com>
2020-05-10 08:00:55 +0100

    [MainGUI] Kotlin -> Java (Kotlin was temperamental with JDK14 preview features)

---
+++ src/main/java/MainGUI.java
@@ -0,0 +1,45 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+
+public class MainGUI extends Application {
+        public static Stage stage;
+
+        public final void main(String args) {
+            Application.launch(new String[]{args});
+        }
+
+        public final void initialiseDictionary() throws URISyntaxException {
+            Extract.readInDictionary();
+        }
+
+        public void start(Stage givenStage) throws URISyntaxException, IOException {
+            stage = givenStage;
+            this.initialiseDictionary();
+            FXMLLoader loader = new FXMLLoader();
+            String fxmlDocPath = "../GUIs/TabbedUI.fxml";
+            FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
+            Object tabPane = loader.load((InputStream)fxmlStream);
+
+            TabPane root = (TabPane)tabPane;
+            Scene scene = new Scene((Parent)root);
+            givenStage.setScene(scene);
+            givenStage.setTitle("HanziToAnki");
+            givenStage.show();
+        }
+
+        public final Stage getStage() {
+            return stage;
+        }
+
+        public final void setStage(Stage newStage) {
+            stage = newStage;
+        }
+}
