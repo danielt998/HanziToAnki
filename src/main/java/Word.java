9acb9d97ff9e8b9ea900130d57ca06e96c1b61e9
James <IdiosApps@users.noreply.github.com>
2020-05-10 08:45:43 +0100

    [Word] Refactor Word to a record

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,53 +1,15 @@
-public class Word {
-    private final int LARGE_PRIME_NUMBER = 65729;
-    private String trad, simp, pinyin, pinTones, def;
-
-    public Word(String givenTrad, String givenSimp, String givenPinyin, String givenPinTones, String givenDef) {
-        trad = givenTrad;
-        simp = givenSimp;
-        pinyin = givenPinyin;
-        pinTones = givenPinTones;
-        def = givenDef;
-    }
-
-    public String getTraditionalChinese() {
-        return trad;
-    }
-
-    public String getSimplifiedChinese() {
-        return simp;
-    }
-
-    public String getPinyinNoTones() {
-        return pinyin;
-    }
-
-    public String getPinyinWithTones() {
-        return pinTones;
-    }
-
-    public String getDefinition() {
-        return def;
-    }
+public record Word(String traditional, String simplified, String pinyin, String pinyinTones, String definition) {
 
     public String getFileOutput() {
-        return trad + ' ' + simp + ' ' + '[' + pinTones + ']' + ' ' + '/' + def;
+        return traditional + ' ' +
+                simplified + ' ' +
+                '[' + pinyinTones + ']' + ' ' +
+                 '/' + definition;
     }
 
     //this allows us to run the sort command to sort the data ;)
     public String getSpecialOutput() {
-        return pinyin + ' ' + trad + ' ' + simp + ' ' + '[' + pinTones + ']' + ' ' + '/' + def;
-    }
-
-    //not sure this makes any difference
-    @Override
-    public int hashCode() {
-  /*  int hash=7;
-    for (int i=0;i<simp.length();i++){
-      hash=hash*LARGE_PRIME_NUMBER + ((int)simp.charAt(i));
-    }
-    return hash;*/
-        return simp.hashCode();
+        return pinyin + ' ' + traditional + ' ' + simplified + ' ' + '[' + pinyinTones + ']' + ' ' + '/' + definition;
     }
 }
 
