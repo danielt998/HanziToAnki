47b7920a58981aeb9f0b72fa9f9a76ad275d6399
James <IdiosApps@users.noreply.github.com>
2021-12-10 12:57:44 +0000

    Revert "Add test for getting tone-marked characters"
    This reverts commit fcb203f94ed81766888b8931d311102f6b9d33c3.

--- src/main/java/hanziToAnki/ToneHelper.java
+++ src/main/java/hanziToAnki/ToneHelper.java
@@ -1,16 +1,14 @@
 package hanziToAnki;
 
-import java.util.List;
-
 public class ToneHelper {
 
-    private static final List<Character> VOWELS_UNACCENTED = List.of('a', 'e', 'i', 'o', 'u', 'ü');
-    private static final List<Character> A_ACCENTs = List.of('ā', 'á', 'ǎ', 'à', 'a');
-    private static final List<Character> E_ACCENTs = List.of('ē', 'é', 'ě', 'è', 'e');
-    private static final List<Character> I_ACCENTs = List.of('ī', 'í', 'ǐ', 'ì', 'i');
-    private static final List<Character> O_ACCENTs = List.of('ō', 'ó', 'ǒ', 'ò', 'o');
-    private static final List<Character> U_ACCENTs = List.of('ū', 'ú', 'ǔ', 'ù', 'u');
-    private static final List<Character> U_UMLAUT_ACCENTs = List.of('ǖ', 'ǘ', 'ǚ', 'ǜ', 'ü');
+    private static final char[] VOWELS_UNACCENTED = new char[]{'a', 'e', 'i', 'o', 'u', 'ü'};
+    private static final char[] A_ACCENTs = new char[]{'ā', 'á', 'ǎ', 'à', 'a'};
+    private static final char[] E_ACCENTs = new char[]{'ē', 'é', 'ě', 'è', 'e'};
+    private static final char[] I_ACCENTs = new char[]{'ī', 'í', 'ǐ', 'ì', 'i'};
+    private static final char[] O_ACCENTs = new char[]{'ō', 'ó', 'ǒ', 'ò', 'o'};
+    private static final char[] U_ACCENTs = new char[]{'ū', 'ú', 'ǔ', 'ù', 'u'};
+    private static final char[] U_UMLAUT_ACCENTs = new char[]{'ǖ', 'ǘ', 'ǚ', 'ǜ', 'ü'};
 
     static char getLetterForTone(String syllable) {
         if (syllable.contains("E")) {
@@ -58,12 +56,12 @@ public class ToneHelper {
             return originalChar;
 
         char c = switch (Character.toLowerCase(originalChar)) {
-            case 'a' -> A_ACCENTs.get(tone - 1);
-            case 'e' -> E_ACCENTs.get(tone - 1);
-            case 'i' -> I_ACCENTs.get(tone - 1);
-            case 'o' -> O_ACCENTs.get(tone - 1);
-            case 'u' -> U_ACCENTs.get(tone - 1);
-            case 'ü' -> U_UMLAUT_ACCENTs.get(tone - 1);
+            case 'a' -> A_ACCENTs[tone - 1];
+            case 'e' -> E_ACCENTs[tone - 1];
+            case 'i' -> I_ACCENTs[tone - 1];
+            case 'o' -> O_ACCENTs[tone - 1];
+            case 'u' -> U_ACCENTs[tone - 1];
+            case 'ü' -> U_UMLAUT_ACCENTs[tone - 1];
             default -> Character.toLowerCase(originalChar);
         };
 
@@ -73,7 +71,11 @@ public class ToneHelper {
     }
 
     private static boolean isCharTonable(char testChar) {
-        return VOWELS_UNACCENTED.stream()
-                .anyMatch(c -> c.equals(testChar));
+        for (char c : VOWELS_UNACCENTED) {
+            if (c == testChar)
+                return true;
+        }
+        return false;
     }
+
 }
