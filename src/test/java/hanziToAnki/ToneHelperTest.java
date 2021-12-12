75b18c5cc1eb4b0d736875bd1b2b561f85267a54
danielt998 <danielt998@users.noreply.github.com>
2021-12-12 22:48:56 +0000

    Update ToneHelperTest

--- src/test/java/hanziToAnki/ToneHelperTest.java
+++ src/test/java/hanziToAnki/ToneHelperTest.java
@@ -6,21 +6,18 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToneHelperTest {
-
     @ParameterizedTest
-    @CsvSource({"a,1,ā",
-                "e,2,é",
-                "i,3,ǐ",
-                "o,4,ò",
-                "u,5,u",
-                "ü,1,ǖ"})
-    void getCharWithTone(Character input, int tone, Character expected) {
-        char result = ToneHelper.getCharWithTone(input, tone);
-        assertEquals(expected, result);
+    @CsvSource({
+            "kan1,kān",
+            "He2,Hé",
+            "pin3,pǐn",
+            "po4,pò",
+            "Wu5,Wu",
+            "lü1,lǖ"
+    })
+    void convertNumberToAccentedSyllable(String inputSyllable, String expectedOutput) {
+        String result = ToneHelper.convertNumberedSyllableToAccentedSyllable(inputSyllable);
+        assertEquals(expectedOutput, result);
     }
 
-//    @Test
-//    void getLetterForTone() {
-//        fail("Not yet implemented");
-//    }
 }
