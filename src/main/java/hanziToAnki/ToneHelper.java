844ecf54f25d7daca45f9e31bc0d6dc4a6fa0dd8
danielt998 <danielt998@users.noreply.github.com>
2021-12-12 22:25:55 +0000

    Tidy up lists of tones a little

--- src/main/java/hanziToAnki/ToneHelper.java
+++ src/main/java/hanziToAnki/ToneHelper.java
@@ -1,14 +1,18 @@
 package hanziToAnki;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ToneHelper {
 
-    private static final char[] VOWELS_UNACCENTED = new char[]{'a', 'e', 'i', 'o', 'u', 'ü'};
-    private static final char[] A_ACCENTs = new char[]{'ā', 'á', 'ǎ', 'à', 'a'};
-    private static final char[] E_ACCENTs = new char[]{'ē', 'é', 'ě', 'è', 'e'};
-    private static final char[] I_ACCENTs = new char[]{'ī', 'í', 'ǐ', 'ì', 'i'};
-    private static final char[] O_ACCENTs = new char[]{'ō', 'ó', 'ǒ', 'ò', 'o'};
-    private static final char[] U_ACCENTs = new char[]{'ū', 'ú', 'ǔ', 'ù', 'u'};
-    private static final char[] U_UMLAUT_ACCENTs = new char[]{'ǖ', 'ǘ', 'ǚ', 'ǜ', 'ü'};
+    private static final Map<Character, char[]> lettersWithToneMarks = new HashMap<>() {{
+        put('a', new char[]{'ā', 'á', 'ǎ', 'à', 'a'});
+        put('e', new char[]{'ē', 'é', 'ě', 'è', 'e'});
+        put('i', new char[]{'ī', 'í', 'ǐ', 'ì', 'i'});
+        put('o', new char[]{'ō', 'ó', 'ǒ', 'ò', 'o'});
+        put('u', new char[]{'ū', 'ú', 'ǔ', 'ù', 'u'});
+        put('ü', new char[]{'ǖ', 'ǘ', 'ǚ', 'ǜ', 'ü'});
+    }};
 
     private static char getCharWithTone(char originalChar, int tone) {
         if (tone < 1 || tone > 5){
@@ -23,27 +27,15 @@ public class ToneHelper {
             return originalChar;
         }
 
-        char c = switch (Character.toLowerCase(originalChar)) {
-            case 'a' -> A_ACCENTs[tone - 1];
-            case 'e' -> E_ACCENTs[tone - 1];
-            case 'i' -> I_ACCENTs[tone - 1];
-            case 'o' -> O_ACCENTs[tone - 1];
-            case 'u' -> U_ACCENTs[tone - 1];
-            case 'ü' -> U_UMLAUT_ACCENTs[tone - 1];
-            default -> Character.toLowerCase(originalChar);
-        };
+        char charAfter = lettersWithToneMarks.get(originalChar)[tone - 1];
 
         if (Character.isLowerCase(originalChar))
-            return c;
-        return Character.toUpperCase(c);
+            return charAfter;
+        return Character.toUpperCase(charAfter);
     }
     
     private static boolean isCharTonable(char testChar) {
-        for (char c : VOWELS_UNACCENTED) {
-            if (c == testChar)
-                return true;
-        }
-        return false;
+        return lettersWithToneMarks.get(testChar) != null;
     }
 
     public static String convertNumberedSyllableToAccentedSyllable(String syllable) {
