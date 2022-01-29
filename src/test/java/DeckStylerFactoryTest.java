618de4ed344eb17d21f65015b9b8af296f36ca02
James <james.sw.clark@gmail.com>
2022-01-29 16:19:34 +0000

    DeckStyler test doesn't use a dictionary extractor now, and ensures word order. DeckStylerFactory test can be improved a bit later, but it should indeed be a test

--- src/test/java/DeckStylerFactoryTest.java
+++ src/test/java/DeckStylerFactoryTest.java
@@ -1,43 +1,13 @@
 import hanziToAnki.DeckStyler;
-import hanziToAnki.DictionaryExtractor;
-import hanziToAnki.chinese.ChineseDictionaryExtractor;
 import hanziToAnki.DeckStylerFactory;
 import hanziToAnki.EmptyDeckStyler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class DeckStylerFactoryTest {
-
-    @Test
-    void testSomeValidWords() throws IOException, URISyntaxException {
-        DictionaryExtractor extractor = new ChineseDictionaryExtractor();
-        extractor.readInDictionary();
-
-        var words = Stream.of("爱", "吧", "阿姨")
-                .map(s-> extractor.getWord(s))
-                .collect(Collectors.toSet());
-
-        DeckStyler deckStyler = DeckStylerFactory.getDeck(words);
-        String deckString = String.join("\n", deckStyler.style(words));
-
-        var resStream = this.getClass().getResourceAsStream("validWordsDeck.txt");
-        var expected = new String(resStream.readAllBytes(), StandardCharsets.UTF_8);
-
-        assertEquals(expected, deckString);
-    }
-
+class DeckStylerFactoryTest {
     @Test
     void noWordsGivesEmptyDeck() {
         DeckStyler deckStyler = DeckStylerFactory.getDeck(null);
         Assertions.assertTrue(deckStyler instanceof EmptyDeckStyler);
     }
-
 }
---
+++ src/test/java/deck_styler/ChineseDeckStylerTest.java
@@ -0,0 +1,38 @@
+package deck_styler;
+
+import hanziToAnki.*;
+import hanziToAnki.chinese.ChineseDeckStyler;
+import hanziToAnki.chinese.ChineseWord;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChineseDeckStylerTest {
+
+    @Test
+    void correctStylingTest() throws IOException {
+        Word aiWord = new ChineseWord("爱", "爱", "ai", "ai4", "to love/to be fond of/to like/affection/to be inclined (to do sth)/to tend to (happen)/");
+        Word baWord = new ChineseWord("吧", "吧", "ba", "ba5", "(modal particle indicating suggestion or surmise)/...right?/...OK?/...I presume./");
+        Word ayiWord = new ChineseWord("阿姨", "阿姨", "ayi", "a1 yi2", "maternal aunt/step-mother/childcare worker/nursemaid/woman of similar age to one's parents (term of address used by child)/CL:個|个[ge4]/");
+
+        LinkedHashSet<Word> words = new LinkedHashSet<>(); // ensures fixed order
+        words.add(aiWord);
+        words.add(baWord);
+        words.add(ayiWord);
+
+        var deckStyler = new ChineseDeckStyler();
+        var styledLines = deckStyler.style(words);
+        String deckString = String.join("\n", styledLines);
+
+        var resStream = this.getClass().getResourceAsStream("/validWordsDeck.txt");
+        var expected = new String(resStream.readAllBytes(), StandardCharsets.UTF_8);
+
+        assertEquals(expected, deckString);
+    }
+
+
+}
