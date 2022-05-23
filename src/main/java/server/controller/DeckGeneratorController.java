ae7e6ba5656257fe7d0ab4c0fa732abcac5b8612
James <james.sw.clark@gmail.com>
2022-05-24 00:09:26 +0100

    FE downloads response as a file

--- src/main/java/server/controller/DeckGeneratorController.java
+++ src/main/java/server/controller/DeckGeneratorController.java
@@ -3,6 +3,8 @@ package server.controller;
 import hanziToAnki.*;
 import hanziToAnki.chinese.ChineseDictionaryExtractor;
 import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,20 @@ import utils.TemporaryFile;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
 import java.nio.file.Files;
 
 @RestController
 public class DeckGeneratorController {
 
     //https://github.com/IdiosApps/gradedReaderBuilderServer/blob/master/src/main/java/com/idiosApps/gradedReaderBuilderServer/BuildController.java
-    @PostMapping("/generate")
-    public void generate(@RequestParam("uploadFile") MultipartFile uploadFile,
+    @PostMapping(value="/generate", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<FileSystemResource> generate(@RequestParam("uploadFile") MultipartFile uploadFile
 //                         @RequestParam("outputType") String outputType,
-                         HttpServletResponse response) throws IOException, URISyntaxException {
+                                      ) throws IOException, URISyntaxException {
 
         // TODO get from UI
         var options = new ExportOptions(true, true, 0, OutputFormat.ANKI);
@@ -37,20 +42,21 @@ public class DeckGeneratorController {
             DeckProducer deckProducer = new DeckProducer(extractor);
 
             var localFile = tempDirectory.getFileFromMultipart(uploadFile);
-            var outputFile = tempDirectory.getFile(".anki");
+            var flashcardFile = tempDirectory.getFile();
 
             var outputLines = deckProducer.produceDeck(
                     localFile.getAbsolutePath(),
                     options
             );
-            FileUtils.writeToFile(outputLines, outputFile.getAbsolutePath());
+            FileUtils.writeToFile(outputLines, flashcardFile.getAbsolutePath());
 
-            IOUtils.copy(new FileInputStream(outputFile), response.getOutputStream());
-            response.flushBuffer();
+            HttpHeaders header = new HttpHeaders();
+            header.setContentType(MediaType.TEXT_PLAIN);
+            header.set(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=" + "flashcards.anki");
+            header.setContentLength(flashcardFile.length());
 
-
-            String contentType = Files.probeContentType(outputFile.toPath());
-            response.setContentType(contentType);
+            return new ResponseEntity(new FileSystemResource(flashcardFile), header, HttpStatus.OK);
         }
     }
 }
