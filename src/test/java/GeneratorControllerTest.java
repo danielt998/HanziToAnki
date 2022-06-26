419ed9382ab4d5b04425688d38f81f51941c4cc2
James <james.sw.clark@gmail.com>
2022-06-26 23:38:20 +0100

    Revert "Should fail on "Extra separation in import""
    This reverts commit 02702ae9f5bb8809a0c342c5d2c9ab29c875b0f6.

--- src/test/java/GeneratorControllerTest.java
+++ src/test/java/GeneratorControllerTest.java
@@ -1,19 +1,14 @@
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import server.Application;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
@@ -23,11 +18,10 @@ public class GeneratorControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void canUploadAWord() throws Exception     {
+    public void canUploadAWord() throws Exception {
         var file = new MockMultipartFile(
                 "uploadFile",
                 "爱".getBytes(StandardCharsets.UTF_8)
-
         );
 
         mockMvc
