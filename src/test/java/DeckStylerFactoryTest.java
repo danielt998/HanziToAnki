a5b2c2864ffd0775a119ae4f165703a76f65138c
daniel <daniel@fearnley.net>
2024-12-07 12:22:29 +0000

    test improvements

--- src/test/java/DeckStylerFactoryTest.java
+++ src/test/java/DeckStylerFactoryTest.java
@@ -3,12 +3,14 @@ import hanziToAnki.DeckStylerFactory;
 import hanziToAnki.EmptyDeckStyler;
 import hanziToAnki.chinese.ChineseDeckStyler;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class DeckStylerFactoryTest {
-    @Test
-    void noWordsGivesEmptyDeck() {
-        DeckStyler deckStyler = DeckStylerFactory.getDeckStyler(null, ChineseDeckStyler.HanziType.SIMP);
+    @ParameterizedTest
+    @EnumSource(ChineseDeckStyler.HanziType.class)
+    void noWordsGivesEmptyDeck(ChineseDeckStyler.HanziType hanziType) {
+        DeckStyler deckStyler = DeckStylerFactory.getDeckStyler(null, hanziType);
         Assertions.assertTrue(deckStyler instanceof EmptyDeckStyler);
     }
 }
--- src/test/java/chinese/DeckStylerTest.java
+++ src/test/java/chinese/DeckStylerTest.java
@@ -8,31 +8,61 @@ import hanziToAnki.chinese.ChineseWord;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashSet;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
 public class DeckStylerTest {
 
-    @Test
-    void correctStylingTest() throws IOException {
-        Word aiWord = new ChineseWord("爱", "爱", "ai", "ai4",
+    private static LinkedHashSet<Word> words = new LinkedHashSet<>();
+
+    @BeforeAll
+    public static void populateWordList() {
+        Word aiWord = new ChineseWord("愛", "爱", "ai", "ai4",
                 "to love/to be fond of/to like/affection/to be inclined (to do sth)/to tend to (happen)/");
         Word baWord = new ChineseWord("吧", "吧", "ba", "ba5",
                 "(modal particle indicating suggestion or surmise)/...right?/...OK?/...I presume./");
         Word ayiWord = new ChineseWord("阿姨", "阿姨", "ayi", "a1 yi2",
                 "maternal aunt/step-mother/childcare worker/nursemaid/woman"
-                       + " of similar age to one's parents (term of address used by child)/CL:個|个[ge4]/");
+                        + " of similar age to one's parents (term of address used by child)/CL:個|个[ge4]/");
 
-        LinkedHashSet<Word> words = new LinkedHashSet<>(); // ensures fixed order
         words.add(aiWord);
         words.add(baWord);
         words.add(ayiWord);
+    }
 
+    @Test
+    void correctStylingTest() throws IOException {
         var deckStyler = new ChineseDeckStyler(ChineseDeckStyler.HanziType.SIMP);
         var styledLines = deckStyler.style(words);
         String deckString = String.join("\n", styledLines);
 
         var resStream = this.getClass().getResourceAsStream("/validWordsDeck.txt");
+        var expected = new String(resStream.readAllBytes(), StandardCharsets.UTF_8);
+
+        assertEquals(expected, deckString);
+    }
+
+    @Test
+    void correctStylingTestWithTrad() throws IOException {
+        var deckStyler = new ChineseDeckStyler(ChineseDeckStyler.HanziType.TRAD);
+        var styledLines = deckStyler.style(words);
+        String deckString = String.join("\n", styledLines);
+
+        var resStream = this.getClass().getResourceAsStream("/validWordsDeckTrad.txt");
+        var expected = new String(resStream.readAllBytes(), StandardCharsets.UTF_8);
+
+        assertEquals(expected, deckString);
+    }
+
+    @Test
+    void correctStylingTestWithSimpAndTrad() throws IOException {
+        var deckStyler = new ChineseDeckStyler(ChineseDeckStyler.HanziType.SIMP_AND_TRAD);
+        var styledLines = deckStyler.style(words);
+        String deckString = String.join("\n", styledLines);
+
+        var resStream = this.getClass().getResourceAsStream("/validWordsDeckSimpAndTrad.txt");
         var expected = new String(resStream.readAllBytes(), StandardCharsets.UTF_8);
 
         assertEquals(expected, deckString);
